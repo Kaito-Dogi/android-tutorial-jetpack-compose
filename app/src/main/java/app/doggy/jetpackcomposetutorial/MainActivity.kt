2d3d98ef4f18606ecf546a1a9a7710463089f79f
Kaito-Dogi <kendobu0405@gmail.com>
2021-08-09 02:15:08 +0900

    add: 画像要素を追加

--- app/src/main/java/app/doggy/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/app/doggy/jetpackcomposetutorial/MainActivity.kt
@@ -2,12 +2,14 @@ package app.doggy.jetpackcomposetutorial
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -32,6 +34,20 @@ fun NewStory() {
         // パラメータを渡してレイアウトの設定を追加できる．
         modifier = Modifier.padding(16.dp)
     ) {
+        // 画像を追加．
+        Image(
+            painter = painterResource(R.drawable.header),
+            modifier = Modifier
+                .height(180.dp)
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+            // contentDescription = null
+        )
+
+        // 余白を追加．
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // 文字を追加．
         Text("A day in Shark Fin Cove")
         Text("Davenport, California")
         Text("December 2018")
