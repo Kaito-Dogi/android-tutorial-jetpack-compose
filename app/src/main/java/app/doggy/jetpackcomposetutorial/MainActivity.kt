430beb04533e94ab3c4b9d47760782bae5c30b82
Kaito-Dogi <kendobu0405@gmail.com>
2021-08-09 02:23:42 +0900

    add: マテリアルデザインらしくするために，画像を角丸にし，文字にMaterialThemeを適用した

--- app/src/main/java/app/doggy/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/app/doggy/jetpackcomposetutorial/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.res.painterResource
@@ -39,7 +42,8 @@ fun NewStory() {
             painter = painterResource(R.drawable.header),
             modifier = Modifier
                 .height(180.dp)
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(4.dp)), // 画像の角をShapeに合わせて切り抜く．
             contentScale = ContentScale.Crop
             // contentDescription = null
         )
@@ -48,9 +52,13 @@ fun NewStory() {
         Spacer(modifier = Modifier.height(16.dp))
 
         // 文字を追加．
-        Text("A day in Shark Fin Cove")
-        Text("Davenport, California")
-        Text("December 2018")
+        Text("A day wandering through the sandhills in Shark Fin Cove, and a few of the sights I saw",
+            style = typography.h6,
+            maxLines = 2)
+        Text("Davenport, California",
+            style = typography.body2)
+        Text("December 2018",
+            style = typography.body2)
     }
 }
 
