54317741e1cc912e2fb587443b888163db5af3bc
Kaito Dogi <kendobu0405@gmail.com>
2022-07-23 17:46:59 +0900

    :sparkles: Text を呼んで画面に文字を表示

--- app/src/main/java/app/doggy/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/app/doggy/jetpackcomposetutorial/MainActivity.kt
@@ -2,9 +2,14 @@ package app.doggy.jetpackcomposetutorial
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            Text("Hello, Jetpack Compose!")
+        }
     }
 }
